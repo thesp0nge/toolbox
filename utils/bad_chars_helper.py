1da1201c21712237e9f9f043a834bf540fac8c1a
Perego Paolo <paolo.perego@gruppomol.it>
2019-01-22 11:23:34 +0100

    Beautify script

--- utils/bad_chars_helper.py
+++ utils/bad_chars_helper.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+#
+# This script write the hex 0-255 representation useful to spot bad characters
+# when writing an exploit.
+
 a=""
 for x in range(1,256):
     if x < 16:
         a+="\\x0"+format(x, "x")
     else:
         a+="\\x"+format(x, "x")
-print a
+print "bad_chars=\"" + a + "\""
