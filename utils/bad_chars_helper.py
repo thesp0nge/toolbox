b118b6f3ea6a9aead4550e0a8d4895999a8eb359
Perego Paolo <paolo.perego@gruppomol.it>
2019-01-22 12:09:04 +0100

    Added some command line features

--- utils/bad_chars_helper.py
+++ utils/bad_chars_helper.py
@@ -2,11 +2,28 @@
 #
 # This script write the hex 0-255 representation useful to spot bad characters
 # when writing an exploit.
+import argparse
+
+parser=argparse.ArgumentParser(description='Generate ASCII characters list in hex format')
+parser.add_argument('-b', '--bad', help='bad characters list')
+parser.add_argument('-v', '--variable', help='specify the variable name to use in output')
+
+args = parser.parse_args()
+
+var_name="bad_chars"
+
+if args.variable is not None:
+  var_name=args.variable
 
 a=""
-for x in range(1,256):
+
+for x in range(0,256):
     if x < 16:
         a+="\\x0"+format(x, "x")
     else:
         a+="\\x"+format(x, "x")
-print "bad_chars=\"" + a + "\""
+if args.bad is None: 
+  print var_name+"=\"" + a + "\""
+else:
+  print var_name+"=\"" + a.replace(args.bad, "") + "\""
+  
